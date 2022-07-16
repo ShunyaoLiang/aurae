f82e3eb5b56db2a41bce592494ed9d023c4c9427
Thomas Liang <thomas.liang1@student.unsw.edu.au>
2022-07-16 17:18:12 +1000

    Add (not-working) buttons

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
+// IDK how this works.
+app.use('/static', express.static('static'));
 app.use(express.static('static'));
 
 app.get('/', (_, res) => {
-  res.sendFile('/index.html')
+  res.sendFile('/static/index.html')
 });
 
 const PORT: number = parseInt(process.env.PORT || config.port);
