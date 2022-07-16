385ef51c98ef738fadf85e131948c3b14397b55b
Thomas Liang <thomas.liang1@student.unsw.edu.au>
2022-07-16 23:08:37 +1000

    who even cares about git history at this point

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
 import { homePageData, Profiledata, reachOutNotif, setMood } from './functions';
 import config from './config.json';
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
+
+// Mount static files in public to be accessible from the prefix static.
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.post('/setMood', (req, res, next) => {
   const { uId, mood } = req.body
@@ -32,8 +38,20 @@ app.get('/reachOut', (req, res, next) => {
   res.json(reachOutNotif(userToken, user));
 });
 
-app.get('/', (_, res) => {
-  res.sendFile('/static/index.html')
+// The home page.
+app.get('/', (req, res) => {
+  const token = req.query.token;
+  // Shockingly, this seems to be the idiomatic way to do this.
+  // Express.js is a travesty of engineering.
+  res.sendFile(path.join(__dirname, '../views/', 'index.html'));
+});
+
+// User profiles.
+app.get('/users/:id', (req, res) => {
+  const token = parseInt(req.query.token as string);
+  const id = parseInt(req.params.id);
+  const profile = Profiledata(token, id);
+  res.render('user', { id: id, profile: profile });
 });
 
 const PORT: number = parseInt(process.env.PORT || config.port);
